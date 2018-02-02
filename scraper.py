3d77c700e13b6ba9297058c1959cb52fb4f57fa0
Lucas Salibian <lucas@freegeekvancouver.org>
2018-02-01 18:04:14 -0800

    a letter

--- scraper.py
+++ scraper.py
@@ -83,7 +83,7 @@ def geocode (companyname,lst):
             coordinate = item.getCoordinate().split(",")
             pnt = kml.newpoint(name=item.getCompany(), coords=[(coordinate[0], coordinate[1], coordinate[2])], description=item.getContents())  # lon, lat, optional height
             setIcon(companyname,pnt)
-            content = json.dump(geocode_result[0]['formatted_address'])
+            content = json.dumps(geocode_result[0]['formatted_address'])
             csv.write(companyname+","+content+"\n")
 
     print("Saving " + companyname + ".kml")
